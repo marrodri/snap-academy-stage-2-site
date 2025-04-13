43b271d4b3e256423ece6b6fc5e6eae0a54a4c3c
marrodri <laza.mar64@gmail.com>
2025-04-13 00:54:54 -0700

    json data fetched and parsed

--- scripts.js
+++ scripts.js
@@ -1,97 +1,82 @@
+
+
+// TODO:
 /**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
+* 1.- bring the json data to the javscript server
+* 2.- show the parsed cards to the front end
+* 3.- create a quick form that you can add a new movie.
+* 4.- create header and footer for better accessibility and sidebar.
+* 5.- create a search bar to filter the movies(this should be on the header).
+* 6.- a sidebar with filter options.
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+*/
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+// parser (parse the collected data to an array of objects.)
+
+let movie_cards = []
+let json_data = []
+
+async function init() {
+    // fetching json data from the data.json local file
+    // await first, before moving to the next step.
+    await fetch('data.json')
+    .then((response) => response.json())
+    .then((json) => (json_data = json));
+
+    for (let i = 0; i < json_data.length; i++) {
+        // creating an object for each movie card
+        // and pushing it to the movie_cards array
+        let movie_card = {
+            title: json_data[i].title,
+            cover_photo: json_data[i].cover_photo,
+            synopsis: json_data[i].synopsis,
+            year: json_data[i].year,
+            genres: json_data[i].genres,
+            actors: json_data[i].actors
+        }
+        //pushing the new created movie card object
+        //to our list of movie cards.
+        movie_cards.push(movie_card);
+    }
+    //checking for one movie card.
+    console.log(movie_cards[0]);
+}
 
 // This function adds cards the page to display the data in the array
 function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
-
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+    const templateCard = document.querySelector(".card");
+  
+    for (let i = 0; i < titles.length; i++) {
+      let title = titles[i];
+  
+      // This part of the code doesn't scale very well! After you add your
+      // own data, you'll need to do something totally different here.
+      let imageURL = "";
+      if (i == 0) {
+        imageURL = FRESH_PRINCE_URL;
+      } else if (i == 1) {
+        imageURL = CURB_POSTER_URL;
+      } else if (i == 2) {
+        imageURL = EAST_LOS_HIGH_POSTER_URL;
+      }
+  
+      const nextCard = templateCard.cloneNode(true); // Copy the template card
+      editCardContent(nextCard, title, imageURL); // Edit title and image
+      cardContainer.appendChild(nextCard); // Add new card to the container
     }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
   }
-}
 
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
-}
-
+// dummy method, we will have to clean it.
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+    // titles.pop(); // Remove last item in titles array
+    // showCards(); // Call showCards again to refresh
+    console.log("Button Clicked YEET!");
+    console.log(json_data)
+  }
+
+
+//   initializing methods
+init();
+  
